4762b3b634214f81640f3557e48f7a60f345f87a
David Giffin <david@giffin.org>
2023-05-10 11:06:33 -0400

    moving the dummy config out of the test directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path("../test/dummy/config/environment", __dir__)
+require File.expand_path("fixtures/dummy/config/environment", __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
